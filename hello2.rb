65856c6114197e7d5b67c2c302bcee8df197df34
John West <john.ryan.west@gmail.com>
2018-05-24 03:11:47 +0000

    created new file hello2.rb in new github repository

---
+++ hello2.rb
@@ -0,0 +1,6 @@
+  gift = [
+          "a partridge in a pair tree",
+          "one gold ring",
+          "two turtle doves",
+         ]
+  puts "On the first day of Christmas my true love gave to me #{gift.sample}."
